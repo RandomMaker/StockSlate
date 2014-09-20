87360caa1da79eb91d6f8bfee82b3ea20ca04730
Jason Kurian <jasonk92@gmail.com>
2014-09-20 02:44:40 -0400

    Added demo livereloading of adding and deleting things.

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -4,19 +4,25 @@ angular.module('stockSlateApp')
   .controller('MainCtrl', function ($scope, $http) {
     $scope.awesomeThings = [];
 
-    $http.get('/api/things').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
-    });
+    $scope.loadThings = function() {
+      $http.get('/api/things').success(function(awesomeThings) {
+        $scope.awesomeThings = awesomeThings;
+      });
+    };
 
     $scope.addThing = function() {
       if($scope.newThing === '') {
         return;
       }
-      $http.post('/api/things', { name: $scope.newThing });
+      $http.post('/api/things', { name: $scope.newThing }).success(function(){
+        $scope.loadThings();
+      });
       $scope.newThing = '';
     };
 
     $scope.deleteThing = function(thing) {
-      $http.delete('/api/things/' + thing._id);
+      $http.delete('/api/things/' + thing._id).success(function(){
+        $scope.loadThings();
+      });
     };
   });
