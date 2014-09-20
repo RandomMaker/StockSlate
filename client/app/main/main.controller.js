475c6fb1717faddbe16a6f2f73fb7aa78341c330
Jason Kurian <jasonk92@gmail.com>
2014-09-20 04:32:32 -0400

    XHRs in main controller for things

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,28 +1,29 @@
 'use strict';
 
-angular.module('stockSlateApp')
-  .controller('MainCtrl', function ($scope, $http) {
-    $scope.awesomeThings = [];
+angular.module('stockSlateApp').controller('MainCtrl', function ($scope, $http) {
+  $scope.awesomeThings = [];
 
-    $scope.loadThings = function() {
-      $http.get('/api/things').success(function(awesomeThings) {
-        $scope.awesomeThings = awesomeThings;
-      });
-    };
+  $scope.loadThings = function() {
+    $http.get('/api/things').success(function(awesomeThings) {
+      $scope.awesomeThings = awesomeThings;
+    });
+  };
 
-    $scope.addThing = function() {
-      if($scope.newThing === '') {
-        return;
-      }
-      $http.post('/api/things', { name: $scope.newThing }).success(function(){
-        $scope.loadThings();
-      });
-      $scope.newThing = '';
-    };
+  $scope.addThing = function() {
+    if($scope.newThing === '') {
+      return;
+    }
+    $http.post('/api/things', { name: $scope.newThing }).success(function() {
+      $scope.loadThings();
+    });
+    $scope.newThing = '';
+  };
 
-    $scope.deleteThing = function(thing) {
-      $http.delete('/api/things/' + thing._id).success(function(){
-        $scope.loadThings();
-      });
-    };
-  });
+  $scope.deleteThing = function(thing) {
+    $http.delete('/api/things/' + thing._id).success(function() {
+      $scope.loadThings();
+    });
+  };
+
+
+});
